1993b400334d7e55665dd00b36db8287ade9312b
pdicko <48698725+pdicko@users.noreply.github.com>
2023-10-28 17:10:32 -0700

    Create update_settings() function to run on 'POST'
    Both the device settings and network settings forms now update
    appropriately. config.ini updates with new settings if 'Apply Changes'
    is clicked. If 'Restore defaul settings' is clicked, the default values
    from config.ini are restored. Next: make serial bridge work within
    current program. Accept settings from config.ini on startup. Will
    eventually need to make the bridge script restart with new settings when
    user applies new settings.

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from config.forms import DeviceSettings, NetworkSettings
 from configparser import ConfigParser
 
@@ -10,12 +10,49 @@ def get_current_settings(section):
     return dict(parser.items(section))
 
 
+def update_settings():
+    form = dict(request.form)
+
+    parser = ConfigParser()
+    config = 'config/config.ini'
+    parser.read(config)
+
+    if 'submit_device_form' in form:
+        section = 'DEVICE-CURRENT'
+        form.popitem()
+        for key in form.keys():
+            parser.set(section, key, form[key])
+    elif 'reset_device_form' in form:
+        section = 'DEVICE-CURRENT'
+        defaults = dict(parser.items('DEVICE-DEFAULTS'))
+        print(f'{defaults}')
+        for key in defaults.keys():
+            parser.set(section, key, defaults[key])
+    elif 'submit_network_form' in form:
+        section = 'NETWORK-CURRENT'
+        form.popitem()
+        for key in form.keys():
+            parser.set(section, key, form[key])
+    elif 'reset_network_form' in form:
+        section = 'NETWORK-CURRENT'
+        defaults = dict(parser.items('NETWORK-DEFAULTS'))
+        print(f'{defaults}')
+        for key in defaults.keys():
+            parser.set(section, key, defaults[key])
+
+    with open(config, 'w') as file:
+        parser.write(file)
+
+
 def open_settings():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '5894d4258a390d7d0ef6b3bfa8861fbd'
 
     @app.route('/', methods=['GET', 'POST'])
     def open_index():
+        if request.method == "POST":
+            update_settings()
+
         current_device_settings = get_current_settings('DEVICE-CURRENT')
         current_network_settings = get_current_settings('NETWORK-CURRENT')
         # will eventually replace with function(s) that get/set network info from
--- config/forms.py
+++ config/forms.py
@@ -7,37 +7,39 @@ class DeviceSettings(FlaskForm):
     def __init__(self, config: dict, **kwargs):
         super().__init__(**kwargs)
         self.config = config
-        self.populate_current_values()
+        self._populate_current_values()
 
     console_ip = StringField('Console IP', validators=[IPAddress(), InputRequired()])
     tcp_port = IntegerField('TCP Port', validators=[InputRequired()])
     serial_device = StringField('Serial device', validators=[InputRequired()])
     baud_rate = IntegerField('Baud rate', validators=[InputRequired()])
-    submit = SubmitField('Apply changes')
+    submit_device_form = SubmitField('Apply changes')
+    reset_device_form = SubmitField('Restore default settings')
 
-    def populate_current_values(self):
+    def _populate_current_values(self):
         if self.config:
             current_config = self.config
-            self.console_ip.data = current_config['console-ip']
-            self.tcp_port.data = current_config['tcp-port']
-            self.serial_device.data = current_config['serial-device']
-            self.baud_rate.data = current_config['baud-rate']
+            self.console_ip.data = current_config['console_ip']
+            self.tcp_port.data = current_config['tcp_port']
+            self.serial_device.data = current_config['serial_device']
+            self.baud_rate.data = current_config['baud_rate']
 
 
 class NetworkSettings(FlaskForm):
     def __init__(self, config: dict, **kwargs):
         super().__init__(**kwargs)
         self.config = config
-        self.populate_current_values()
+        self._populate_current_values()
 
     ip = StringField('IP', validators=[IPAddress(), InputRequired()], default='10.101.60.101')
     subnet_mask = StringField('Subnet mask', validators=[IPAddress(), InputRequired()], default='255.255.0.0')
     gateway = StringField('Gateway', validators=[IPAddress(), InputRequired()], default='10.101.1.1')
-    submit = SubmitField('Apply changes')
+    submit_network_form = SubmitField('Apply changes')
+    reset_network_form = SubmitField('Restore default settings')
 
-    def populate_current_values(self):
+    def _populate_current_values(self):
         if self.config:
             current_config = self.config
             self.ip.data = current_config['ip']
-            self.subnet_mask.data = current_config['subnet-mask']
+            self.subnet_mask.data = current_config['subnet_mask']
             self.gateway.data = current_config['gateway']
